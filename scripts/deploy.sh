d4b1c5e4eb5efda4f5bb16d708b62b8afaf47e5c
ZerataX <mail@zera.tax>
2018-05-11 01:16:17 +0200

    travis rsync don't check hostkey

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $SERVER_USER@$SERVER:$SERVER_DIR
+rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $SERVER_USER@$SERVER:$SERVER_DIR
 ssh  -o "StrictHostKeyChecking no" $SERVER_USER@$SERVER:$SERVER_DIR chmod 0755 $SERVER_DIR -R
