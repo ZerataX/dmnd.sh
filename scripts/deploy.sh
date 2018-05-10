74142132e6c598666baa94921f184a5dd6f19ec9
ZerataX <mail@zera.tax>
2018-05-11 01:35:01 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $SERVER_USER@$SERVER:$SERVER_DIR
-ssh  -o "StrictHostKeyChecking no" $SERVER_USER@$SERVER:$SERVER_DIR chmod 0755 $SERVER_DIR -R
+ssh  -o "StrictHostKeyChecking no" $SERVER_USER@$SERVER chmod 0755 $SERVER_DIR -R
