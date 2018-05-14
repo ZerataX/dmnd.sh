c24e96d06d5b2dacc41b0ab84fdad45a21b906c9
ZerataX <mail@zera.tax>
2018-05-14 03:48:42 +0200

    added registering

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,45 @@
-import message from './diamond';
-
-console.log(message);
+import { getParameterByName } from './url_query'
 
 export function cube(x) {
   return x**3;
+}
+
+window.onload = function () {
+  // set token input to "?token=" query parameter
+  document.getElementById('token').value = getParameterByName("token");
+
+  // html5 validators
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  var confirm_password = document.getElementById("confirm_password");
+  var token = document.getElementById("token");
+
+  username.addEventListener("input", function (event) {
+    if (username.validity.typeMismatch) {
+      username.setCustomValidity("format: @username:matrix.org");
+    } else {
+      username.setCustomValidity("");
+    }
+  });
+
+  token.addEventListener("input", function (event) {
+    if (token.validity.typeMismatch) {
+      token.setCustomValidity("case-sensitive, e.g: SardineImpactReport");
+    } else {
+      token.setCustomValidity("");
+    }
+  });
+
+  function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("passwords don't match");
+    } else {
+      confirm_password.setCustomValidity("");
+    }
+  }
+
+
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword
+
 }
--- test/test.js
+++ test/test.js
@@ -1,18 +1,4 @@
 import assert from 'assert';
-import { cube } from '../src/scripts/index';
 
-describe('Array', ()=> {
-  describe('#indexOf()', ()=> {
-    it('should return -1 when the value is not present', ()=> {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
-
-describe('main', ()=> {
-  describe('#cube()', ()=> {
-    it('should return 9 when the value is 3', ()=> {
-      assert.equal(cube(3), 27);
-    });
-  });
-});
+// TODO phantom-js tests
+// https://github.com/nathanboktae/mocha-phantomjs
