7bd51e6c4f194f212b95d7f6085bf9ffb46350ca
ZerataX <mail@zera.tax>
2018-06-08 23:02:06 +0200

    update test script

--- test/test.js
+++ test/test.js
@@ -1,4 +1,13 @@
 import assert from 'assert';
+import diamond from '../src/scripts/diamond.js'
+
+describe('test', ()=> {
+  describe('#test()', ()=> {
+    it('this is just a test test', ()=> {
+      assert.equal(diamond, 'DIAMONDS');
+    });
+  });
+});
 
 // TODO phantom-js tests
 // https://github.com/nathanboktae/mocha-phantomjs
